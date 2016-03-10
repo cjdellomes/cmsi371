7d4c71f5f850aa4315ce91b0579defc7ae592175
Chris Dellomes <cjdellomes@gmail.com>
2016-03-09 17:16:30 -0800

    Scene animation done

--- toon/keyframe-tweening-demo.js
+++ toon/keyframe-tweening-demo.js
@@ -157,6 +157,44 @@
                         lidAngle: -1*Math.PI/180,
                         color: "orange"
                     }
+                },
+
+                {
+                    frame: 380,
+                    tx: 610,
+                    ty: 350,
+                    parameters: {
+                        lidHeight: 20,
+                        lidAngle: -1 * Math.PI/180,
+                        color: "orange"
+                    }
+                }
+            ]
+        },
+
+        {
+            draw: SpriteLibrary.table,
+            keyframes: [
+                {
+                    frame: 0,
+                    tx: 610,
+                    ty: 500,
+                    ease: KeyframeTweener.quadEaseIn,
+                    parameters: {
+                        tableDimensions: { w: 300, h: 10 },
+                        legDimensions: { w: 10, h: 50 },
+                    }
+                },
+
+                {
+                    frame: 380,
+                    tx: 610,
+                    ty: 500,
+                    ease: KeyframeTweener.quadEaseIn,
+                    parameters: {
+                        tableDimensions: { w: 300, h: 10 },
+                        legDimensions: { w: 10, h: 50 },
+                    }
                 }
             ]
         },
@@ -332,37 +370,200 @@
                         startingAngle: Math.PI * 1.5,
                         endingAngle: Math.PI * 0.5
                     }
-                }
+                },
 
-            ]
-        },
-
-        {
-            draw: SpriteLibrary.table,
-            keyframes: [
                 {
-                    frame: 0,
-                    tx: 610,
+                    frame: 380,
+                    tx: 600,
                     ty: 500,
-                    ease: KeyframeTweener.quadEaseIn,
+                    sx: 2,
+                    sy: 2,
                     parameters: {
-                        tableDimensions: { w: 300, h: 10 },
-                        legDimensions: { w: 10, h: 50 },
-                        color: "green"
+                        startingAngle: Math.PI * 2,
+                        endingAngle: Math.PI * 0.00001
                     }
                 },
 
                 {
-                    frame: 300,
-                    tx: 610,
+                    frame: 430,
+                    tx: 600,
                     ty: 500,
-                    ease: KeyframeTweener.quadEaseIn,
+                    sx: 2,
+                    sy: 2,
                     parameters: {
-                        tableDimensions: { w: 300, h: 10 },
-                        legDimensions: { w: 10, h: 50 },
-                        color: "green"
+                        startingAngle: Math.PI * 2,
+                        endingAngle: Math.PI * 0.00001
+                    }
+                },
+
+                {
+                    frame: 540,
+                    tx: 330,
+                    ty: 300,
+                    sx: 0.5,
+                    sy: 0.5,
+                    parameters: {
+                        startingAngle: Math.PI * 2,
+                        endingAngle: Math.PI * 0.00001
+                    }
+                },
+
+                {
+                    frame: 580,
+                    tx: 330,
+                    ty: 300,
+                    sx: 0.5,
+                    sy: 0.5,
+                    rotate: 180,
+                    parameters: {
+                        startingAngle: Math.PI * 1.5,
+                        endingAngle: Math.PI * 0.5
+                    }
+                },
+
+                {
+                    frame: 590,
+                    tx: 300,
+                    ty: 300,
+                    sx: 0.5,
+                    sy: 0.5,
+                    rotate: 180,
+                    parameters: {
+                        startingAngle: Math.PI * 1.99,
+                        endingAngle: Math.PI * 0.01
+                    }
+                },
+
+                {
+                    frame: 600,
+                    tx: 270,
+                    ty: 300,
+                    sx: 0.4,
+                    sy: 0.4,
+                    rotate: 180,
+                    parameters: {
+                        startingAngle: Math.PI * 1.5,
+                        endingAngle: Math.PI * 0.5
+                    }
+                },
+
+                {
+                    frame: 610,
+                    tx: 240,
+                    ty: 300,
+                    sx: 0.3,
+                    sy: 0.3,
+                    rotate: 180,
+                    parameters: {
+                        startingAngle: Math.PI * 1.99,
+                        endingAngle: Math.PI * 0.01
+                    }
+                },
+
+                {
+                    frame: 620,
+                    tx: 210,
+                    ty: 300,
+                    sx: 0.2,
+                    sy: 0.2,
+                    rotate: 180,
+                    parameters: {
+                        startingAngle: Math.PI * 1.5,
+                        endingAngle: Math.PI * 0.5
+                    }
+                },
+
+                {
+                    frame: 630,
+                    tx: 180,
+                    ty: 300,
+                    sx: 0.1,
+                    sy: 0.1,
+                    rotate: 180,
+                    parameters: {
+                        startingAngle: Math.PI * 1.99,
+                        endingAngle: Math.PI * 0.01
+                    }
+                },
+
+                {
+                    frame: 640,
+                    tx: 150,
+                    ty: 300,
+                    sx: 0.1,
+                    sy: 0.1,
+                    rotate: 180,
+                    parameters: {
+                        startingAngle: Math.PI * 1.5,
+                        endingAngle: Math.PI * 0.5
+                    }
+                },
+
+                {
+                    frame: 650,
+                    tx: 120,
+                    ty: 300,
+                    sx: 0.1,
+                    sy: 0.1,
+                    rotate: 180,
+                    parameters: {
+                        startingAngle: Math.PI * 1.99,
+                        endingAngle: Math.PI * 0.01
+                    }
+                },
+
+                {
+                    frame: 660,
+                    tx: 90,
+                    ty: 300,
+                    sx: 0.1,
+                    sy: 0.1,
+                    rotate: 180,
+                    parameters: {
+                        startingAngle: Math.PI * 1.5,
+                        endingAngle: Math.PI * 0.5
+                    }
+                },
+
+                {
+                    frame: 670,
+                    tx: 60,
+                    ty: 300,
+                    sx: 0.1,
+                    sy: 0.1,
+                    rotate: 180,
+                    parameters: {
+                        startingAngle: Math.PI * 1.99,
+                        endingAngle: Math.PI * 0.01
+                    }
+                },
+
+                {
+                    frame: 680,
+                    tx: 30,
+                    ty: 300,
+                    sx: 0.1,
+                    sy: 0.1,
+                    rotate: 180,
+                    parameters: {
+                        startingAngle: Math.PI * 1.5,
+                        endingAngle: Math.PI * 0.5
+                    }
+                },
+
+                {
+                    frame: 690,
+                    tx: 0,
+                    ty: 300,
+                    sx: 0.1,
+                    sy: 0.1,
+                    rotate: 180,
+                    parameters: {
+                        startingAngle: Math.PI * 1.99,
+                        endingAngle: Math.PI * 0.01
                     }
                 }
+
             ]
         }
     ];
